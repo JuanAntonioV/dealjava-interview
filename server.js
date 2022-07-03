bb4c2cb25114d041b59fdc6dbc5960f3cabc3381
Juan Antonio V <juananthonio23@gmail.com>
2022-07-03 22:19:44 +0700

    Fixing Server Js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('data/data.json');
 const middlewares = jsonServer.defaults({
-  static: './build',
+  static: './dist',
 });
 const port = process.env.PORT || 3001;
 
